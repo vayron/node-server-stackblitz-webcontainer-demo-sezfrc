8bc59c4e0f99f80acddd9253382d3e912792a252
vayron <123513544@qq.com>
2022-07-21 22:05:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-server-stackblitz-webcontainer-demo-p8pbpf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-server-stackblitz-webcontainer-demo-p8pbpf)
---
+++ server.js
@@ -0,0 +1,14 @@
+import { request } from 'undici';
+
+const { statusCode, headers, trailers, body } = await request(
+  'http://localhost:3000/foo'
+);
+
+console.log('response received', statusCode);
+console.log('headers', headers);
+
+for await (const data of body) {
+  console.log('data', data);
+}
+
+console.log('trailers', trailers);
